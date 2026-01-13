e60551eb72c4c5459797f1d7a0775e243b6ac5ca
Guilherme Bechtold <gbechtold91@gmail.com>
2026-01-13 01:54:44 +0000

    Segunda Versao teste

--- Questao 3.java
+++ Questao 3.java
@@ -20,6 +20,8 @@ public class Matriz {
         C[1][0] = A[1][0]*B[0][0] + A[1][1]*B[1][0] + A[1][2]*B[2][0]
         C[1][1] = A[1][0]*B[0][1] + A[1][1]*B[1][1] + A[1][2]*B[2][1]
 
+        //Teste de push
+
         System.out.println(C[0][0]) + C[0][1]);
         System.out.println(C[1][0]) + C[1][1]);
     }
